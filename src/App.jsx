79ee5af91ed325dc0d80f64e5a2894d9d6931dda
Victor Hugo Faria Vanelli <vhvanelli@gmail.com>
2021-10-01 10:52:46 -0300

    Performance optimization attempt

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 
-// import MilkaOreo from "https://i.imgur.com/ZKuDNWq.png"
-// import MilkaConfetti from "https://i.imgur.com/g4Fddg9.png"
-// import MilkaMousse from "https://i.imgur.com/eXzFC1j.png";
-// import MilkaStrawberry from "https://i.imgur.com/7cZoEqS.png";
-
 import { BodyContainer, BoxContainer } from './styles'
 
 const App = () => {
@@ -17,28 +12,28 @@ const App = () => {
       img: "https://i.imgur.com/ZKuDNWq.png",
       alt: "chocolate-sabor-oreo",
       description: "Chocolate ao leite recheado com pedaços de Oreo",
-      price:"R$ 12,99"
+      price: "R$ 12,99"
     },
     {
       flavor: "Confetti",
       img: "https://i.imgur.com/g4Fddg9.png",
       alt: "chocolate-sabor-confete",
       description: "Chocolate ao leite com confetes",
-      price:"R$ 11,99"
+      price: "R$ 11,99"
     },
     {
       flavor: "Mousse",
       img: "https://i.imgur.com/eXzFC1j.png",
       alt: "chocolate-sabor-mousse",
       description: "Chocolate ao leite recheado com um mousse de chocolate",
-      price:"R$ 13,99"
+      price: "R$ 13,99"
     },
     {
       flavor: "Strawberry",
       img: "https://i.imgur.com/7cZoEqS.png",
       alt: "chocolate-sabor-morango",
       description: "Chocolate ao leite recheado com pedaços de morango",
-      price:"R$ 14,99"
+      price: "R$ 14,99"
     },
   ];
 
